f68df67afda29144b15116aad7f8853f3e830b9b
vsuraj25 <55409076+vsuraj25@users.noreply.github.com>
2022-09-27 01:44:24 +0530

    Data Transformation Configuration and updated EDA.ipynb

--- demo.py
+++ demo.py
@@ -9,10 +9,10 @@ from housing.config.configuration import Configuration
 
 def main():
     try:
-        pipeline = Pipeline()
-        pipeline.run_pipeline()
-        # data_validation_config = Configuration().get_data_validation_config()
-        # print(data_validation_config)
+        # pipeline = Pipeline()
+        # pipeline.run_pipeline()
+        data_transformation_config = Configuration().get_data_transformation_config()
+        print(data_transformation_config)
 
     except Exception as e:
         raise HousingException(e,sys) from e
--- housing/entity/artifact_entity.py
+++ housing/entity/artifact_entity.py
@@ -6,3 +6,7 @@ DataIngestionArtifact = namedtuple("DataIngestionArtifact", ['train_file_path', 
 
 DataValidationArtifact = namedtuple("DataValidationArtifact", ["schema_file_path", "report_file_path", "report_page_file_path",
                                     "is_validated", "message"])
+
+DataTransformationArtifact = namedtuple("DataTransformationArtifact", ["is_transformed", "message", "transformed_train_file_path",
+                                    "transformed_test_file_path", "processed_object_file_path"])
+
